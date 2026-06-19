d9cf03de3905a9c0e251eab14b69d372a241b12d
SaiSnehasree <medasaisneha@gmail.com>
2026-06-19 10:43:26 +0530

    Initial Spring Boot project setup

---
+++ logisticsmarketplace/src/main/java/com/sneha/logisticsmarketplace/LogisticsmarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.sneha.logisticsmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsmarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsmarketplaceApplication.class, args);
+	}
+
+}
---
+++ logisticsmarketplace/src/test/java/com/sneha/logisticsmarketplace/LogisticsmarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.logisticsmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsmarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
